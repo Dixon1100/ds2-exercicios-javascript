5a660c70594267e5ee8e8ae84c3aa160e1f325d7
Dixon1100 <dixontaberasfurones@gmail.com>
2026-08-26 09:15:41 -0300

    arrumando caminho dos js no script do index

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -1,1 +1,8 @@
-const aluno = "Dixon"
+const aluno = "Dixon Ernesto Taberas Furones";
+const curso ="Sistemas para Internet";
+const funcionalidade = "Validação de formularios em tempo real";
+
+console.log("JavaScript carregado com sucesso");
+console.log(aluno);
+console.log(curso);
+console.log(funcionalidade);
